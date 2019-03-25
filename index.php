0e8b0ae93c09280c0c9933192ce6bdf198186fc5
Andre Monteiro <admxxi@gmail.com>
2019-03-25 00:55:10 +0000

    Moving from controller to router script

--- index.php
+++ index.php
@@ -1,16 +1,57 @@
 <?php
 require_once "./vendor/autoload.php";
 
-use Http\Request;
-use Http\Response;
-
+use Core\Controller;
 
 try {
-    $http = new Request(new Response);
-    $http->render($http->getData(), "json");
-    exit;
+    $route = "\\Core\\Controller";
+    $action = false;
+
+    if (isset($_SERVER["PATH_INFO"])) {
+        $pathInfo = $_SERVER["PATH_INFO"];
+        $split_path = array_values(array_filter(explode("/", $pathInfo)));
+
+
+        if (count($split_path) === 1) {
+            $route = "\\Core\\Controller";
+            $action = $split_path[0];
+        }
+        if (count($split_path) > 1) {
+            $route = "\\Route\\" . $split_path[0] . "Controller";
+            $action = $split_path[1];
+        }
+    }
+
+    $controller = new Controller();
+    $controller->setRoute($route);
+    $controller->setAction($action);
+
+    if (!class_exists($controller->getRoute())) {
+        throw new ErrorException("Route {$controller->getRoute()} not found");
+    }
+
+    $className = $controller->getRoute();
+
+    if ($action && class_exists($className)) {
+        $routeObject = new $className();
+        $controller->setRoute($route);
+
+        if (!method_exists($routeObject, $controller->getAction())) {
+            throw new ErrorException("{$controller->getRoute()}->{$controller->getAction()} Action not found");
+        }
+
+        $controller->setAction($action);
+        $action_name = $controller->getAction();
+        $routeObject->$action_name();
+        exit;
+    }
+
+    $controller->setAction("Default");
+    $controller->actionDefault();
+
 } catch (ErrorException $e) {
-    $http = new Request(new Response);
-    $http->render([], "json");
-    exit;
+    $controller->getRequest()->getResponse()->setStatus($e->getCode());
+    $controller->render($e->getMessage());
 }
+
+exit;
